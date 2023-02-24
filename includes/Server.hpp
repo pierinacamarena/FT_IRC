6b8e4f7470e6bc97adb5737508a52e42c4e3ef91
pierinacamarena <pcamaren@student.42.fr>
2023-02-24 02:44:25 +0100

    server class is working

---
+++ includes/Server.hpp
@@ -0,0 +1,67 @@
+#ifndef _SERVER_HPP_
+#define _SERVER_HPP_
+
+#include <netdb.h>
+#include <cstring>
+#include <string>
+#include <iostream>
+#include <unistd.h>
+
+class Server
+{
+	private:
+		struct addrinfo hints;
+		struct addrinfo *servinfo;
+		struct addrinfo *p;
+		int				listener;
+		int				yes;
+
+	public:
+		Server(const char *port)
+		{
+			yes = 1;
+			memset(&hints, 0, sizeof(hints));
+			hints.ai_family = AF_UNSPEC;
+			hints.ai_socktype = SOCK_STREAM;
+			hints.ai_flags = AI_PASSIVE;
+
+			int status;
+			if ((status = getaddrinfo(NULL, port, &hints, &servinfo)) != 0)
+			{
+				perror("getaddrinfo:");
+				exit(1);
+			}
+			for (p = servinfo; p != NULL; p = p->ai_next)
+			{
+				if ((listener = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) < 0)
+					continue;
+				setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+				if ((status = bind(listener, p->ai_addr, p->ai_addrlen)) == -1)
+					continue;
+				break;
+			}
+			freeaddrinfo(servinfo);
+			if (p == NULL)
+			{
+				std::cerr << "ERROR: failed to bind socket" << std::endl;
+				exit(1);
+			}
+			if (listen(listener, 10) == -1)
+			{
+				std::cerr << "ERROR: failed to listen on socket" << std::endl;
+				exit(1);
+			}
+		}
+
+		~Server()
+		{
+			close(listener);
+		}
+
+		int	get_listener(void) const
+		{
+			return listener;
+		}
+};
+
+#endif
