6941154748101030bd4f309990a420f183fbf11b
pierinacamarena <pcamaren@student.42.fr>
2023-02-26 20:37:21 +0100

    SockeServer: removed unnecessary includes

--- includes/SocketServer.hpp
+++ includes/SocketServer.hpp
@@ -1,12 +1,7 @@
 #ifndef _SOCKETSERVER_HPP_
 #define _SOCKETSERVER_HPP_
 
-#include <netdb.h>
-#include <cstring>
-#include <string>
-#include <iostream>
-#include <unistd.h>
-// #define
+#include "headers.hpp"
 
 class SocketServer
 {
@@ -15,15 +10,15 @@ class SocketServer
 		struct addrinfo *servinfo;
 		struct addrinfo *p;
 		int				listener;
-		int				yes = 1;
+		int				yes;
 		const char*		_port;
-		char*		_psswd;
+		// char*		_psswd;
 
 		void	socket_bind();
 
 		void	socket_listen();
 	public:
-		SocketServer(const char *port,char *psswd);
+		SocketServer(const char *port);
 
 		virtual ~SocketServer();
 
