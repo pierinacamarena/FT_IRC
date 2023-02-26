b629ba44712656e3d05fbb95b3fa3493da835c8c
pierinacamarena <pcamaren@student.42.fr>
2023-02-26 20:36:37 +0100

    header.hpp: added for cleanliness

---
+++ includes/headers.hpp
@@ -0,0 +1,23 @@
+#ifndef _HEADERS_HPP_
+#define _HEADERS_HPP_
+
+#include <netdb.h>
+#include <cstring>
+#include <string>
+#include <iostream>
+#include <unistd.h>
+#include <errno.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <vector>
+#include <poll.h>
+#include <iostream>
+
+#endif
