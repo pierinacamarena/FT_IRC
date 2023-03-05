29ac62090a8399c3c20651f707b167a57c55e142
pierinacamarena <pcamaren@student.42.fr>
2023-03-05 17:50:15 +0100

    deleted redundant macros

--- includes/Command.hpp
+++ includes/Command.hpp
@@ -5,23 +5,8 @@
 
 #include "Data.hpp"
 #include "cmd_structs.hpp"
-
-// REPLIES
-#define RPL_WELCOME 1
-#define RPL_UMODEIS 221
-
-// ERROR REPLIES
-#define ERR_UNKNOWNCOMMAND 421
-#define ERR_NONICKNAMEGIVEN 431
-#define ERR_ERRONEUSNICKNAME 432
-#define ERR_NICKNAMEINUSE 433
-#define ERR_NOTREGISTERED 451
-#define ERR_NEEDMOREPARAMS 461
-#define ERR_ALREADYREGISTERED 462
-#define ERR_PASSWDMISMATCH 464
-#define ERR_RESTRICTED 484
-#define ERR_UMODEUNKNOWNFLAG 501
-#define ERR_USERSDONTMATCH 502
+#include "headers.hpp"
+#include "parser.hpp"
 
 class	Command {
 
