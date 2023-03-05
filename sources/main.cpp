c5942d0773b1d14dde75606b722bf6bc349f636d
pierinacamarena <pcamaren@student.42.fr>
2023-03-05 17:51:42 +0100

    main: calling data class

--- sources/main.cpp
+++ sources/main.cpp
@@ -6,12 +6,13 @@
 /*   By: pcamaren <pcamaren@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/26 19:46:46 by pcamaren          #+#    #+#             */
-/*   Updated: 2023/02/26 20:28:47 by pcamaren         ###   ########.fr       */
+/*   Updated: 2023/03/05 16:52:31 by pcamaren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/SocketServer.hpp"
 #include "../includes/Server.hpp"
+#include "../includes/Data.hpp"
 
 
 int main(int ac, char **av)
@@ -21,9 +22,10 @@ int main(int ac, char **av)
 		std::cerr << "usage: ./ircserv <port> <password>";
 		exit(1);
 	}
+	Data data(av[2]);
 	SocketServer socket_server(av[1]);
 	int	listener = socket_server.get_listener();
-	Server server(listener, av[2]);
+	Server server(listener, &data);
 	server.run();
 	return 0;
 }
