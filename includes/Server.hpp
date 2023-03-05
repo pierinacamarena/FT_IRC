77be3640713f06c12c96a35341ffd4fc625d94bb
pierinacamarena <pcamaren@student.42.fr>
2023-03-05 13:48:42 +0100

    server: deleted the client map

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -7,11 +7,12 @@
 class Server
 {
 	private:
-		std::vector<struct pollfd>	_pfds;
-		int 						_listener;
-		char						buff[256];
-		std::string 				_password;
-		std::map<int, Client*>		_clients;
+		std::vector<struct pollfd>			_pfds;
+		int 								_listener;
+		char								buff[256];
+		std::string 						_password;
+		std::map<int, Client*>				_clients;
+		// std::map<std::string, Channel*>		_channels;
 
 
 	public:
@@ -20,19 +21,19 @@ class Server
 
 	void add_to_pfds(int new_fd);
 
-	void del_from_pfds(int i);
+	void del_from_pfds(pfd_iter iter);
 
 	int	get_fd(int i);
 
 	void	*get_in_addr(struct sockaddr *sa);
 
-	void	accept_connection(size_t location);
+	int		accept_connection(size_t location);
 
-	void	receive_send_data(size_t location);
+	int		receive_send_data(pfd_iter iter);
 
 	void	send_data(int numbytes, int sender_fd);
 
-	int		receive_data(size_t location, int *numbytes);
+	int		receive_data(pfd_iter iter, int *numbytes);
 
 	void run();
 
