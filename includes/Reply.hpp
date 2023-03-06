5815042248f85a48bdbbf20a73914c8f98120def
pierinacamarena <pcamaren@student.42.fr>
2023-03-06 21:34:53 +0100

    reply.hpp: modified the fucntion parameters

--- includes/Reply.hpp
+++ includes/Reply.hpp
@@ -4,31 +4,31 @@
 #include "headers.hpp"
 
 /*******************************ERRORS***********************************/
-void	error_caller(int reply_id, const std::set<int>& dest_fds, const std::string &command, const std::string &identifyer);
+void	error_caller(int reply_id, const std::set<int>& dest_fds, const std::vector<std::string>& args);
 
-void	err_unknown_command(int dest_fd, const std::string &command);
-void	err_need_moreparams(int dest_fd, const std::string &command);
-void	err_already_registered(int dest_fd, const std::string &identifyer);
-void	err_not_registered(int dest_fd, const std::string &identifyer);
-void	err_no_nicknamegive(int dest_fd, const std::string &identifyer);
-void	err_erroneus_nickname(int dest_fd, const std::string &nickname, const std::string &identifyer);
-void	err_nickname_inuse(int dest_fd, const std::string &nickname, const std::string &identifyer);
-void	err_restricted(int dest_fd, const std::string &identifyer);
-void	err_nooperhost(int dest_fd, const std::string &identifyer);
-void	err_passwd_mistmatch(int dest_fd, const std::string &identifyer);
-void	err_umode_unknownflag(int dest_fd, const std::string &identifyer);
+void	err_unknown_command(int dest_fd, const std::vector<std::string> args);
+void	err_need_moreparams(int dest_fd, const std::vector<std::string> args);
+void	err_already_registered(int dest_fd, const std::vector<std::string> args);
+void	err_not_registered(int dest_fd, const std::vector<std::string> args);
+void	err_no_nicknamegive(int dest_fd, const std::vector<std::string> args);
+void	err_erroneus_nickname(int dest_fd, const std::vector<std::string> args);
+void	err_nickname_inuse(int dest_fd, const std::vector<std::string> args);
+void	err_restricted(int dest_fd, const std::vector<std::string> args);
+void	err_nooperhost(int dest_fd, const std::vector<std::string> args);
+void	err_passwd_mistmatch(int dest_fd, const std::vector<std::string> args);
+void	err_umode_unknownflag(int dest_fd, const std::vector<std::string> args);
 void	err_users_dontmatch(int dest_fd);
-void	err_invite_onlychan(int dest_fd, const std::string &channel, const std::string &identifyer);
-void	err_channel_isfull(int dest_fd, const std::string &channel, const std::string &identifyer);
-void	err_nosuch_channel(int dest_fd, const std::string &channel, const std::string &identifyer);
-void	err_toomany_targets(int dest_fd, const std::string &target, const std::string &identifyer);
+void	err_invite_onlychan(int dest_fd, const std::vector<std::string> args);
+void	err_channel_isfull(int dest_fd, const std::vector<std::string> args);
+void	err_nosuch_channel(int dest_fd, const std::vector<std::string> args);
+void	err_toomany_targets(int dest_fd, const std::vector<std::string> args);
 
 /******************************REPLIES***********************************/
 
-void	reply_caller(int reply_id, const std::set<int> dest_fds, const std::string &identifyer);
-void	rpl_welcome_message(int dest_fd, const std::string &identifyer);
-void	rpl_umodeis(int dest_fd, const std::string& identifyer);
-void	rpl_youreoper(int dest_fd, const std::string &identifyer);
+void	reply_caller(int reply_id, const std::set<int> dest_fds, const std::vector<std::string>& args);
+void	rpl_welcome_message(int dest_fd, const std::vector<std::string> args);
+void	rpl_umodeis(int dest_fd, const std::string identifyer);
+void	rpl_youreoper(int dest_fd, const std::vector<std::string> args);
 
 
 #endif
